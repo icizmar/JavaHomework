6627d6aee5cc29e441030b2d19f5d7829bfc30a2
icizmar <33398648+icizmar@users.noreply.github.com>
2018-01-12 22:47:53 +0100

    InchooIspravan

--- Inchoo/Inchoo.java
+++ Inchoo/Inchoo.java
@@ -1,44 +1,80 @@
 package vjezbanje;
 
-public class Inchoo {
+////ZADATAK:
+//Na osnovu unesenih redova i kolona generirati tablicu u kojoj
+//u doljem desnom uglu se poèinje s brojem jedan. Brojevi se uveæavaju za 1 
+// u smjeru kazaljke na satu sve do broja redova*kolona.
+import javax.swing.JOptionPane;
+
+public class Inchoo { 
 	
-	//Prva proba
-
+	
 	public static void main(String[] args) {
-
-		int[][] niz = new int[4][4];
-		int DD = 0;
-		int redci = 5;
-		int stupci = 5;
-		int i = niz.length - 1;
-		int j = 0;
 		
-			
-		for (j = niz.length-1; j >= 0; j--) {
-			niz[i][j] = ++DD;
+		int brojRedaka = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
+		int brojStupaca = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));;
+		int kraj = brojRedaka*brojStupaca;
+		int[][] niz=new int[brojRedaka][brojStupaca];		
+		int element = 0;
+		int i = brojRedaka-1;
+		int j = brojStupaca;
+		int DDj = brojStupaca-1;
+		int DDi = brojRedaka-1;
+		int DLi = brojRedaka-1;
+		int GDi = 0;
+		int GLi = 0;
+		int DLj = 0;
+		int GDj = brojStupaca-1;
+		int GLj = 0;
+		
+		do{
+		j--;
+		for (j = DDj; j >= DLj; j--) {     
+			niz[i][j] = ++element;
 		}
 		j++;
-
-		for (i = ((niz.length - 1) - 1); i >= 0; i--) {
-			niz[i][j] = ++DD;
+		if(element==kraj) {  
+			break;
+		}
+		
+		for (i = DLi - 1; i >= GLi; i--) {
+			niz[i][j] = ++element;
 		}
 		i++;
-
-		for (j = 1; j <= niz.length - 1; j++) {
-			niz[i][j] = ++DD;
+		if(element==kraj) {  
+			break;
+		}
+		
+		for (j = GLj + 1; j <= GDj; j++) {
+			niz[i][j] = ++element;
 		}
 		j--;
-
-		for (i = 1; i <= ((niz.length - 1) - 1); i++) {
-			niz[i][j] = ++DD;
+		if(element==kraj) {  
+			break;
+		}
+		
+		for (i = GDi + 1; i <= (DDi - 1); i++) {
+			niz[i][j] = ++element;
 		}
 		i--;
-		j--;
-		niz[i][j] = ++DD;
-		
+		if(element==kraj) {  //prekida prije nego smanji j za jos jedan i preko veæ postojalog èlana niza ga napise
+			break;
+		}
 
-		for (i = 0; i < niz.length; i++) {
-			for (j = 0; j < niz.length; j++) {
+		DDi -= 1;   // Pomjeranje granica jer nakon svakog kruga dobijemo manju matricu
+		DDj -= 1; 
+		DLi -= 1; 
+		DLj += 1;
+		GLi += 1;
+		GLj += 1;
+		GDi += 1;
+		GDj -= 1;
+					
+		}while(element<kraj);
+		
+//    ISPIS **************************************************** ispisuje lijepu matricu ukoliko su elementi max troznamenkasti
+		for (i = 0; i < brojRedaka; i++) {
+			for (j = 0; j < brojStupaca; j++) {
 				
 				if (niz[i][j] < 10) {
 					System.out.print(niz[i][j] + "   ");
@@ -50,7 +86,8 @@ public class Inchoo {
 			}
 			System.out.println();
 		}
+		
 
+		
 	}
-
 }
